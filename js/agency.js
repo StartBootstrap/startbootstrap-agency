8dd956c249ccf197ec7200f3b46056c6ce9a41b3
David Miller <david@ironsummitmedia.com>
2014-10-30 20:59:32 -0400

    Revert to basic theme and 3.3.0 update

--- js/agency.js
+++ js/agency.js
@@ -1,3 +1,9 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
