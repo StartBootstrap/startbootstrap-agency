b8360de43c0da466fe53bee6eb7d8209fdbc75b5
meinhard <meinhard@benn.org>
2015-01-25 14:54:50 +0100

    Speed up easing

--- js/agency.js
+++ js/agency.js
@@ -10,7 +10,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
