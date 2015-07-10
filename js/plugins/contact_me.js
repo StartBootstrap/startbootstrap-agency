6158488073acd2114ec34e99b0c60c86778bd782
Pierre LASSIALE <pierre.lassiale@gmail.com>
2015-07-10 17:55:30 +0200

    Maj du formulaire de contact

--- js/plugins/contact_me.js
+++ js/plugins/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Votre message a été envoyé. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,10 +45,8 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Erreur lors de l'envoi de votre message, merci de re-essayer plus tart ou contactez nous directement contact@ma-boite-a-fleurs.fr</strong>");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
                 },
             })
         },
