542b2e21dab1bdebd7560ccf95c7479917eb0f83
Pierre LASSIALE <pierre.lassiale@gmail.com>
2015-09-17 15:07:06 +0200

    Revert "VP Noel init"
    This reverts commit 92a86004d92d4ef5902589d52ef345a887d17da9.

--- js/custom-cookies.js
+++ js/custom-cookies.js
@@ -184,8 +184,8 @@ tagAnalyticsCNIL.CookieConsent = function() {
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      //  ga('create', gaProperty, 'auto');
-      //  ga('send', 'pageview');
+        ga('create', gaProperty, 'auto');
+        ga('send', 'pageview');
 
 
     }
