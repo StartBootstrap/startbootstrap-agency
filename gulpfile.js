cee1a3605e8ed8a9bc1098c9ae3f76ac5f015e21
David Miller <hello@davidmiller.io>
2016-06-22 16:39:59 -0400

    updated banners and minified CSS files using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,61 @@
+// Assigning modules to local variables
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var pkg = require('./package.json');
 
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
+// Default task
 gulp.task('default', function() {
     // default tasks here
 });
 
+// Less task to compile the less files and add the banner
 gulp.task('less', function() {
     return gulp.src('less/agency.less')
         .pipe(less())
+        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
 
+// Minify CSS
+gulp.task('minify-css', function() {
+    return gulp.src('css/agency.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Grab Bootstrap core files from node_modules
 gulp.task('bootstrap', function() {
     return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest(''))
 })
 
+// Grab jQuery core files from node_modules
 gulp.task('jquery', function() {
     return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('js'))
 })
 
+// Grab Font Awesome core files from node_modules
 gulp.task('fontawesome', function() {
     return gulp.src([
             'node_modules/font-awesome/**',
@@ -37,8 +68,10 @@ gulp.task('fontawesome', function() {
         .pipe(gulp.dest('font-awesome'))
 })
 
+// Updates all dependencies and puts them into their proper destinations
 gulp.task('update', ['bootstrap', 'jquery', 'fontawesome']);
 
+// Configure the browserSync task
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
@@ -47,9 +80,10 @@ gulp.task('browserSync', function() {
     })
 })
 
-// Watch Task
-gulp.task('watch', ['browserSync', 'less'], function() {
+// Watch Task that compiles LESS and watches for HTML or JS changes and reloads with browserSync
+gulp.task('watch', ['browserSync', 'less', 'minify-css'], function() {
     gulp.watch('less/*.less', ['less']);
+    gulp.watch('css/*.css', ['minify-css']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
