dc0e1f196feb4ab8bd8dbbb16d93fdf90d22c805
Rahav lussato <rahav@yallo.com>
2016-07-08 14:09:30 +0300

    change email

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "../mail/contact_me.php",
+                url: "https://formspree.io/contact@orhanona.com",
                 type: "POST",
                 data: {
                     name: name,
