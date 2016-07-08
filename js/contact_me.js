d2e9609bf4c5aead70269adaf6661c96eac547a2
Rahav lussato <rahav@yallo.com>
2016-07-08 13:59:34 +0300

    change mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "../mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
