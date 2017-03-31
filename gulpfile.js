7877676292227038e7aed7719bacccc347373c9d
David Miller <hello@davidmiller.io>
2017-03-31 17:58:28 -0400

    fix banner

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var pkg = require('./package.json');
 var banner = ['/*!\n',
     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
     ' */\n',
     ''
 ].join('');
