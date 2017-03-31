50616c3b88c8573275108a16652d27d93bd7bb7a
David Miller <hello@davidmiller.io>
2017-03-31 18:16:22 -0400

    minor text fix

--- js/agency.js
+++ js/agency.js
@@ -1,7 +1,7 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery Easing
+    // Smooth scrolling using jQuery easing
     $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
