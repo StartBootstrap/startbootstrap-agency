ef6c31d6bab5f1df78782224fa1a52fa947959be
FireEmerald <fire.github@gmail.com>
2017-07-01 20:57:38 +0200

    Fixed error dialog not showing up if the contact message wasn't send.

--- js/contact_me.js
+++ js/contact_me.js
@@ -29,29 +29,12 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                success: function(result) {
+                    processResult(result);
                 },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    processResult("error");
+                }
             });
         },
         filter: function() {
@@ -63,6 +46,28 @@ $(function() {
         e.preventDefault();
         $(this).tab("show");
     });
+
+    var processResult = function(result) {
+        if (result == "success") {
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#success > .alert-success')
+                .append("<strong>Your message has been sent. </strong>");
+            $('#success > .alert-success')
+                .append('</div>');
+        } else {
+            // Fail message
+            $('#success').html("<div class='alert alert-danger'>");
+            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#success > .alert-danger').append($("<strong>").text("Sorry, it seems that the mail server is not responding. Please try again later!"));
+            $('#success > .alert-danger').append('</div>');
+        }
+        //clear all fields
+        $('#contactForm').trigger("reset");
+    }
 });
 
 
