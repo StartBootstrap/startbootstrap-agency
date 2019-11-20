2454d23e740fd9844a5622744ab200b33c6b3d9e
lisasiurina <lisa.siurina@gmail.com>
2019-11-20 12:46:20 +0100

    xx

---
+++ svgtry/map-config-blank.js
@@ -0,0 +1,82 @@
+﻿// This is similar to "map-config.js" with basic settings (colors, info., etc)
+var tryjsconfig = {
+  "culo":{
+    "hover": "AREA #1",//info of the popup
+    "url": "https://www.html5interactivemaps.com/",//link to any webpage
+    "target": "same_window",// use "new_window", "same_window", "modal", or "none"
+    "upColor": "#FFFFF3",//default color
+    "overColor": "#ECFFB3",//highlight color
+    "downColor": "#cae9af",//clicking color
+    "active": true//true/false to activate/deactivate
+  },
+  "tryjs2":{
+    "hover": "AREA #2",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs3":{
+    "hover": "AREA #3",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs4":{
+    "hover": "AREA #4",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs5":{
+    "hover": "AREA #5",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs6":{
+    "hover": "AREA #6",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs7":{
+    "hover": "AREA #7",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs8":{
+    "hover": "AREA #8",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs9":{
+    "hover": "AREA #9",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs10":{
+    "hover": "AREA #10",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs11":{
+    "hover": "AREA #11",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs12":{
+    "hover": "AREA #12",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "general":{
+    "borderColor": "#9CA8B6",
+    "visibleNames": "#adadad"
+  }
+};
---
+++ svgtry/mapconfig.js
@@ -0,0 +1,81 @@
+var tryjsconfig = {
+  "culo":{
+    "hover": "AREA #1",//info of the popup
+    "url": "https://www.html5interactivemaps.com/",//link to any webpage
+    "target": "same_window",// use "new_window", "same_window", "modal", or "none"
+    "upColor": "#FFFFF3",//default color
+    "overColor": "#ECFFB3",//highlight color
+    "downColor": "#cae9af",//clicking color
+    "active": true//true/false to activate/deactivate
+  },
+  "tryjs2":{
+    "hover": "<b><u>AREA #2</u></b><br>Write any text and load images<br><img src='example.png'>",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#f1ffc8", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs3":{
+    "hover": "<b><u>AREA #3</u></b><br><span style='color: #bcbcbc;'>Street Address:</span><br>&nbsp;321 Example, Address 54321<br><span style='color: #bcbcbc;'>Telephone:</span><br>&nbsp;(256) 555-4321 / (256) 555-1234",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#d7f57a", "overColor": "#beef2a", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs4":{
+    "hover": "<b><u>AREA #4</u></b><br><span style='color: #999;'>*Click to open a webpage*</span>",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#f1ffc8", "overColor": "#d9ff66", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs5":{
+    "hover": "<b><u>AREA #5</u></b><br><span style='color: #999;'>Click to open a modal window!</span><br><span style='color: #ff6666;'><b>Modal Window Option is Compatible<br> with Bootstrap Only.</b></span>",
+    "url": "#mymodal", "target": "modal",
+    "upColor": "#edff66", "overColor": "#cbe600", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs6":{
+    "hover": "AREA #6",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#E0E2E2", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": false
+  },
+  "tryjs7":{
+    "hover": "AREA #7",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs8":{
+    "hover": "AREA #8",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs9":{
+    "hover": "AREA #9",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs10":{
+    "hover": "AREA #10",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs11":{
+    "hover": "AREA #11",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs12":{
+    "hover": "AREA #12",
+    "url": "https://www.html5interactivemaps.com/", "target": "same_window",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "general":{
+    "borderColor": "#9CA8B6",
+    "visibleNames": "#adadad"
+  }
+};
