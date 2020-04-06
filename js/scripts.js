f460340c753792f6ba2d19816d060baf1c3d6b83
David Miller <hello@davidmiller.io>
2020-04-06 18:19:58 -0400

    initial v6 structural changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-(function($) {
+/*!
+    * Start Bootstrap - Agency v6.0.0 (https://startbootstrap.com/template-overviews/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
