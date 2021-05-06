8c4b7f0128966deae8ef4350bf78c7c6b193a984
Sam Artioli <initplatform@gmail.com>
2021-05-05 21:56:26 -0400

    close responsiveNav after click for mobile

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -33,4 +33,15 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
+    // Collapse responsive navbar when toggler is visible. 
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            navbarToggler.click();
+        });
+    });
+
 });
